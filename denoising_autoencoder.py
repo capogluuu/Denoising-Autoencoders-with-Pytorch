34364451a5acd273583ec7d76520d20993f2e145
capogluuu <capogluuu@gmail.com>
2021-03-27 12:03:17 +0300

    Update denoising_autoencoder.py

--- denoising_autoencoder.py
+++ denoising_autoencoder.py
@@ -5,17 +5,6 @@
 import numpy as np # linear algebra
 import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
 
-# Input data files are available in the read-only "../input/" directory
-# For example, running this (by clicking run or pressing Shift+Enter) will list all files under the input directory
-
-import os
-for dirname, _, filenames in os.walk('/kaggle/input'):
-    for filename in filenames:
-        print(os.path.join(dirname, filename))
-
-# You can write up to 20GB to the current directory (/kaggle/working/) that gets preserved as output when you create a version using "Save & Run All" 
-# You can also write temporary files to /kaggle/temp/, but they won't be saved outside of the current session
-
 import matplotlib.pyplot as plt
 import seaborn as sns
 
@@ -32,6 +21,15 @@ import torch.optim as optim
 from torchvision import datasets, transforms
 
 sns.set_style('darkgrid')
+
+# Input data files are available in the read-only "../input/" directory
+# For example, running this (by clicking run or pressing Shift+Enter) will list all files under the input directory
+
+for dirname, _, filenames in os.walk('/kaggle/input'):
+    for filename in filenames:
+        print(os.path.join(dirname, filename))
+
+
 
 
 
